f947b23198149fd6d58d891368066ff8d129dc97
kunalvishnoi <vishnoi.kunal0@gmail.com>
2019-03-11 00:53:48 +0530

    project upload

---
+++ MiCM_Project_Match_selection_test/src/index.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      picture:[] ,
+      loading: true
+    };
+  }
+ 
+ componentWillMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      this.setState({
+        picture:data,
+        loading: false
+        })
+      
+    })
+   }
+   render() {
+     if (this.state.loading) {
+       return (
+      <div>
+      <h1 className="heading">Users(loading)</h1></div>
+       )
+    }
+    else
+    return (
+    <div>
+    <h1 className="heading">Users</h1>
+    <div className="container">
+    <div className="left">
+    {this.state.picture.map(data => {
+          return (
+              <div key={data._id}>
+                <h3 style={{paddingRight:'5px'}} className="tooltip">
+                  { data.name}
+                  <span className="tooltiptext"><b>Street</b> {data.address.street}
+                  <br/>
+                  <b>Suite</b> {data.address.suite}
+                  <br/>
+                  <b>City</b> {data.address.city}
+                  <br/>
+                  <b>Zip</b> {data.address.zipcode}
+                  </span>
+                </h3>
+                </div>
+              
+          );
+        })}
+        </div>
+        <div className="right">
+        {this.state.picture.map(data => {
+          return (
+              <div key={data._id}>
+                <p>
+                  ({data.username})
+                </p>
+                </div>
+              
+          );
+        })}
+        </div>
+        </div>
+    </div>
+    );
+   }
+}
+
+render(<App />, document.getElementById('root'));
